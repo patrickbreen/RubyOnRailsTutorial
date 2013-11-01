58f81c0ed35dad90567ce5cc515c09d5a00e3409
tecleatu <pbreen.uga.edu>
2013-10-31 21:14:56 -0400

    modified readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Tutorial::Application.routes.draw do
-  get "users/new"
-  root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
+  root  'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/home',    to: 'static_pages#home',    via: 'get'
